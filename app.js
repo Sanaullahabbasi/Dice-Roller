c0c156fa0cd849c5e559f02bc3a412fcbdad1fe2
Sanaullahabbasi <136949915+Sanaullahabbasi@users.noreply.github.com>
2023-11-07 22:35:12 +0500

    work in progress about programming

--- app.js
+++ app.js
@@ -7,16 +7,22 @@ function img() {
 var imgMotion = setInterval(img, 2500);
 
 function roll() {
-    var clearBtn = document.getElementById("btn-clear");
-  clearInterval(imgMotion);
+  var rollBtn = document.getElementById("btn-roll");
   var imgAnimation = document.querySelector("#dice>img");
   var dicVal = document.getElementById("diceVal");
+  var clearBtn = document.getElementById("btn-clear");
+  clearBtn.style.display = "block";
+  clearInterval(imgMotion);
+  rollBtn.style.display = "none";
   imgAnimation.src = "images/blank.png";
   dicVal.innerHTML = `
   <h1>${random}</h1>
-  `
-  }
+  `;
+}
 
-  function rollAgain(){
-
-  }
+function rollAgain() {
+  var clearBtn = document.getElementById("btn-clear");
+  var rollBtn = document.getElementById("btn-roll");
+  rollBtn.style.display = "block";
+  clearBtn.style.display = "none";
+}
